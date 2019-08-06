ac13de74d5426c2f69c3cc7a75751b31e2f75eb1
vvinod <vvinod@tavisca.in>
2019-08-06 17:29:14 +0530

    Initial commit

---
+++ src/main/java/DigitFinder.java
@@ -0,0 +1,20 @@
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
+public class DigitFinder {
+    public static int FindDigit(String args) {
+        String[] terms =args.split( "\\*|=" );
+        /*if (terms[0].Contains('?'))
+        {
+            return (FirstTermHasMissingDigit(terms));
+        }
+        if (terms[1].Contains('?'))
+        {
+            return (SecondTermHasMissingDigit(terms));
+        }
+        if (terms[2].Contains('?'))
+        {
+            return (RHSHasMissingDigit(terms));
+        }*/
+        return -1;
+    }
+}
---
+++ src/main/java/Equation.java
@@ -0,0 +1,11 @@
+public class Equation {
+    int firstTerm;
+    int secondTerm;
+    int rightHandSide;
+    public Equation()
+    {
+        this.firstTerm=0;
+        this.secondTerm=0;
+        this.rightHandSide=0;
+    }
+}
